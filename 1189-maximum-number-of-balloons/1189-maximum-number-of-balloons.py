2aa53fc5fa30cb8d1251bbf3a69f1d63a65a71e2
Nikhil Parekh <30530766+nikhilparekh@users.noreply.github.com>
2022-01-25 21:37:47 -0800

    Time: 48 ms (34.72%), Space: 14.4 MB (20.69%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        di = {'b':0,'a':0,'l':0,'o':0,'n':0}
+        for i in text:
+            if i in di:
+                di[i]+=1
+        di['l']//=2
+        di['o']//=2
+        mn = float("inf")
+        for i in di:
+            if di[i]<mn:
+                mn = di[i]
+        return mn
+            
