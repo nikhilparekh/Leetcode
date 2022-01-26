c3e1d7b095b60ba1d7c744df6360c44e4cf88446
Nikhil Parekh <30530766+nikhilparekh@users.noreply.github.com>
2022-01-25 21:38:17 -0800

    Time: 45 ms (38.41%), Space: 14.4 MB (51.96%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,5 +1,7 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
+        if len(text)<7:
+            return 0
         di = {'b':0,'a':0,'l':0,'o':0,'n':0}
         for i in text:
             if i in di:
